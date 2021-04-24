a79f2c4eb6acf70d8b85e09b5fef003e747e0ff2
SmashedFrenzy16 <68993968+SmashedFrenzy16@users.noreply.github.com>
2021-04-24 15:18:30 +0100

    Create archery.py

---
+++ archery.py
@@ -0,0 +1,28 @@
+from turtle import *
+import time
+
+speed(1)
+shape("turtle")
+penup()
+dot(200)
+
+color('white')
+dot(150)
+
+color('black')
+dot(100)
+color('white')
+dot(50)
+
+color('red')
+dot(25)
+
+color('blue')
+goto(110,45)
+stamp()
+time.sleep(2)
+goto(-45,-60)
+stamp()
+time.sleep(2)
+goto(0,0)
+stamp()
